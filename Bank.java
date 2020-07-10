892c71e4d2409654d382ca8a464372b46867c9f8
alexlempka <31479707+alexlempka@users.noreply.github.com>
2020-07-10 10:58:24 +0200

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,56 @@
+package com.jogwal;
+
+import java.util.ArrayList;
+
+public class Bank {
+	ArrayList<Customer> customers = new ArrayList<Customer>();
+	public void addCustomer(Customer customer) {
+		customers.add(customer);
+	}
+	Customer getCustomer(int account) {
+		return customers.get(account);
+	}
+	ArrayList<Customer> getCustomers(){
+		return customers;
+	}
+}
+//______________________________________________________________________________________________________________________
+class Customer{
+	private final String firstName;
+	private final String lastName;
+	private final String socialSecurityNumber;
+	private final Account account;
+
+	public Customer(String firstName, String lastName, String socialSecurityNumber, Account account) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.socialSecurityNumber = socialSecurityNumber;
+		this.account = account;
+	}
+	public Customer(String firstName, String middleName, String lastName, String socialSecurityNumber, Account account) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.socialSecurityNumber = socialSecurityNumber;
+		this.account = account;
+	}
+	Account getAccount(){
+		return account;
+	}
+	@Override
+	public String toString() {
+		return "\ncustomer information\n" +
+				"firsName : " + firstName +
+				"lastName: " + lastName +
+				"socialSecurityNumber" + socialSecurityNumber +
+				account
+				;
+	}
+	public String basicInformation() {
+		return "\ncustomer information\n" +
+				" firsName : " + firstName + "\n" +
+				" lastName: " + lastName + "\n" +
+				" socialSecurityNumber" + socialSecurityNumber + "\n" +
+				" Acccount Number: " + account.getAccountNumber()
+				;
+	}
+}
